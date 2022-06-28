92ee0c2376e5fcc08bf332f5a46c236288a01338
Haile-08 <haile4cmd@gmail.com>
2022-06-28 16:39:18 +0000

    prints the ASCII alphabet, in lowercase, not followed by a new line

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(ord("a"), ord("z")):
+    if chr(i) == 'q':
+        continue
+    elif chr(i) == 'e':
+        continue
+    else:
+        print("{:c}".format(i))
