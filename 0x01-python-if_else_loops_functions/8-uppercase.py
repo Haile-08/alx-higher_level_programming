f36d7e786f56007e8e703e7e5f0a49d31a881bac
Haile-08 <haile4cmd@gmail.com>
2022-06-28 18:38:17 +0000

    changes

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in str:
         if ord('a') <= ord(i) <= ord('z'):
-            upper = chr(ord(i) - 32)
-            print("{:s}".format(upper), end='')
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end='')
     print("")
