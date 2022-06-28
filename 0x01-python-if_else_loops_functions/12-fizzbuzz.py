ba19f6f0b55333c51f5b2258e0cc3d3ca0139c0d
Haile-08 <haile4cmd@gmail.com>
2022-06-28 18:57:55 +0000

    edit the code style

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,100):
+    for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz", end='')
         if i % 5 == 0:
@@ -8,4 +8,3 @@ def fizzbuzz():
         if i % 3 and i % 5:
             print("{:d}".format(i), end='')
         print(end=' ')
-
