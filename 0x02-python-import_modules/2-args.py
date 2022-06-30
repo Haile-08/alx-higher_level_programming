fe1b6e5eae1c1c6835ca10622bca8dec71e0c429
Haile-08 <haile4cmd@gmail.com>
2022-06-30 18:49:12 +0000

    program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-import sys
-print("{} argument:".format(len(sys.argv)-1))
-for i in range(len(sys.argv)):
-    if i == 0:
-        continue
+from sys import argv
+def principal():
+    print('{} argument'.format(len(argv) - 1), end='')
+    if len(argv) == 1:
+        print('s.')
+    elif len(argv) == 2:
+        print(':')
     else:
-        print("{}: {}".format(i, sys.argv[i]))
+        print('s:')
+    for i in range(1, len(argv)):
+        print('{}: {}'.format(i, argv[i]))
+if __name__ == "__main__":
+    principal()
