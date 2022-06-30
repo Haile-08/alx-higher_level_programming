f9247e75271962f3c4040e236e98107ed722454a
Haile-08 <haile4cmd@gmail.com>
2022-06-30 18:45:50 +0000

    removed some format function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
-print("{:d} argument:".format(len(sys.argv)-1))
+print("{} argument:".format(len(sys.argv)-1))
 for i in range(len(sys.argv)):
     if i == 0:
         continue
     else:
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
