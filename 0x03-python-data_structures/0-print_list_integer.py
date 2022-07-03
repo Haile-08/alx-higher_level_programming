2ccec0a77342650802ddbcfba6091a21897e4b0f
Haile-08 <haile4cmd@gmail.com>
2022-07-03 10:49:24 +0000

    try 1 for 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        print("{}\n".format(my_list))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
