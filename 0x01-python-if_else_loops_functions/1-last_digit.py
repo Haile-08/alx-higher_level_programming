76276393a75fd964f7a2ab0b3f3de571835a67f6
Haile-08 <haile4cmd@gmail.com>
2022-11-30 02:24:41 -0800

    Handele negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    last_digit = (-1) * (number % 10)
-else:
+if number > 0:
     last_digit = number % 10
-text = 'Last digit of {:d} is {:d}'.format(number, last_digit)
-if last_digit > 5:
-    print('{} and is greater than 5'.format(text))
-elif last_digit == 0:
-    print('{} and is 0'.format(text))
 else:
-    print('{} and is less than 6 and not 0'.format(text))
+    last_digit = number % (-10)
+text = 'Last digit of {:d}'.format(number)
+if last_digit > 5:
+    print('{} is {:d} and is greater than 5'.format(text, last_digit))
+elif last_digit == 0:
+    print('{} is {:d} and is 0'.format(text, last_digit))
+else:
+    print('{} is {:=d} and is less than 6 and not 0'.format(text, last_digit))
