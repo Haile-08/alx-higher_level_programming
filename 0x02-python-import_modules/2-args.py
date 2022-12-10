d442411e14fe9c029bcb3b8f8cbecec38ec54db5
Haile-08 <haile4cmd@gmail.com>
2022-12-10 02:36:43 -0800

    Cast to string

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         else:
             print('{:d} arguments:'.format(len(argv) - 1))
     for i in range(1, len(argv)):
-        print('{:d}: {}'.format(i, argv[i]))
+        print('{:d}: {:s}'.format(i, argv[i]))
