9a371818545083f5679bb9aab3db2b572237be15
Haile-08 <haile4cmd@gmail.com>
2022-12-10 02:31:03 -0800

    Argument followed by : (or . if no arguments were passed) followed by

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 if __name__ == "__main__":
-    lists = sys.argv
-    print('{:d} argument:'.format(len(lists) - 1))
-    for i in range(1, len(lists)):
-        print('{:d}: {}'.format(i, lists[i]))
+    if len(argv) == 1:
+        print('{:d} argument.'.format(len(argv) - 1))
+    else:
+        print('{:d} argument:'.format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print('{:d}: {}'.format(i, argv[i]))
