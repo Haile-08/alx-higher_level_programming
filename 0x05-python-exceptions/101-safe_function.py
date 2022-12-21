9f3b183acc177341755b4c6fef2ff89ef29af1b9
Haile-08 <haile4cmd@gmail.com>
2022-12-21 04:13:02 -0800

    Add else statemnt

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,8 @@ import sys
 def safe_function(fct, *args):
     try:
         num = fct(args[0], args[1])
-        return num
     except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return None
+    else:
+        return num
