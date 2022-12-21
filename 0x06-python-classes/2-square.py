deedd34db1d37da99d2481191add7c8eaa8cfd5e
Haile-08 <haile4cmd@gmail.com>
2022-12-21 05:30:13 -0800

    Remove the try except

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,10 +5,8 @@
 class Square:
     """Class named square"""
     def __init__(self, size=0):
-        try:
-            int(size)
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except TypeError:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
