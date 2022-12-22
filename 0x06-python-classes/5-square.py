a2e5049bf337e441b015f12ef173a1d3df07acad
Haile-08 <haile4cmd@gmail.com>
2022-12-22 04:27:44 -0800

    Public instance method: def my_print(self): that prints in stdout the square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import sys
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """initalize the class.
+
+        args:
+            size: the size of the square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """A getter method."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A setter method.
+        args:
+            vlaue: the value to set.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """The area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print a #."""
+        if self.__size > 0:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", file=sys.stdout, end="")
+                print("")
+        elif self.__size == 0:
+            print("")
