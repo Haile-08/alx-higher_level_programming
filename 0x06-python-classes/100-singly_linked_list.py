686c84b1106ce3bea335b1fde29e842eaf0d71dd
Haile-08 <haile4cmd@gmail.com>
2022-12-29 03:02:54 -0800

    Write a class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,7 +6,7 @@ class Node:
     """Represent a Likned list node"""
 
     def __init__(self, data, next_node=None):
-        """initalize the node class
+        """initalize the data
         args:
             data: the data input
             next_node: the next node input
@@ -45,4 +45,32 @@ class SinglyLinkedList:
     """Represent a singly Linked list"""
 
     def __init__(self):
+        """Initialize the data"""
+        self.__head = None
 
+    def sorted_insert(self, value):
+        """Method to sorted insert
+        args:
+            value: the input data
+        """
+        new_node = Node(value)
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            temp = self.__head
+            while (temp.next_node is not None and temp.next_node.data < value):
+                temp = temp.next_node
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+    def __str__(self):
+        """Define the print() representaion of a linked list"""
+        value = []
+        temp = self.__head
+        while temp is not None:
+            value.append(str(temp.data))
+            temp = temp.next_node
+        return ("\n".join(value))
