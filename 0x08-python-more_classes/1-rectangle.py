0593ad01c8301417e126bd6d6bc866d351add4ea
Haile-08 <haile4cmd@gmail.com>
2023-01-03 00:14:48 -0800

    Make the error text from value to width and height in both the getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -38,9 +38,9 @@ class Rectangle:
             value: integer input
         """
         if not isinstance(value, int):
-            raise TypeError("value must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("value must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -58,7 +58,7 @@ class Rectangle:
             value: integer input
         """
         if not isinstance(value, int):
-            raise TypeError("value must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("value must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
