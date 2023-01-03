0b3821eb2b6176252a5dc83714390f1d73ce75c3
Haile-08 <haile4cmd@gmail.com>
2023-01-03 00:04:39 -0800

    Add the checker back to the instance variable

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,6 +12,14 @@ class Rectangle:
             width: width integer input
             height: height integer input
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.width = width
         self.height = height
 
