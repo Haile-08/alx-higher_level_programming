b3c5aeb44ca98cfb4645d6ab971a7742180c4ae9
Haile-08 <haile4cmd@gmail.com>
2023-01-03 00:00:00 -0800

    Change the the instance from private to public

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width: width integer input
             height: height integer input
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
