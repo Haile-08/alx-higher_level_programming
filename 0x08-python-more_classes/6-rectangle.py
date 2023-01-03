9c45f66f8568f6bfe7c6bbc65b458144a3c088d0
Haile-08 <haile4cmd@gmail.com>
2023-01-03 03:42:20 -0800

    Create a class variable to count the instance of a class

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class Rectangle:
+    """Represent a rectangle"""
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initilize the class
+        Args:
+            width: integer input
+            height: integer input
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """A getter method for width
+        Returns:
+            the width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """A setter method for width
+        Args:
+            value: integer input
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """A getter method for height
+        Returns:
+            the height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """A setter method for height
+        Args:
+            value: integer input
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        """Area of the rectangle
+        Returns:
+            the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Area of the reactangle
+        Returns:
+            the perimeter of the reactangle
+            if height or width == 0 return 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Define the a print() represntaion of the class"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        else:
+            for i in range(self.__height):
+                [print("#", end="") for i in range(self.__width)]
+                if i != self.__height - 1:
+                    print("")
+        return ("")
+
+    def __repr__(self):
+        """Return the class rep"""
+        strw = str(self.__width)
+        strh = str(self.__height)
+        return "Rectangle(" + strw + ", " + strh + ")"
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
