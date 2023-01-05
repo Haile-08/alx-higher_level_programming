bb8f0bc9c7bd1a1768aa648ef2b7ce883bd965cc
Haile-08 <haile4cmd@gmail.com>
2023-01-05 04:35:01 -0800

    Write a function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+    """print square #
+    Args:
+        size: the length of the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
