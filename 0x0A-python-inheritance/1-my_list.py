6e16a5c04920e6c594db734e8e403ef57c60c9bb
Haile-08 <haile4cmd@gmail.com>
2023-01-09 05:39:48 -0800

    Write a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define our own mylist"""
+
+
+class MyList(list):
+    """Represent a custom list"""
+
+    def __init__(self, a=[]):
+        if not isinstance(a, list):
+            raise TypeError("Input should be a list")
+        for i in a:
+            if type(i) not in [int, float]:
+                raise ValueError("Element should be int or float")
+        for i in a:
+            self.append(i)
+    def print_sorted(self):
+        """Print the sorted list"""
+        print(sorted(self))
