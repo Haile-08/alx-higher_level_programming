059a0d2ffdbb7be9003938215a04e583bd023a4e
Haile-08 <haile4cmd@gmail.com>
2023-01-10 04:14:19 -0800

    Enter a documentaion

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-import os
+"""Define a function that read file"""
+
 
 def read_file(filename=""):
+    """Read file
+    Args:
+        filename: string input
+    """
     with open(filename, encoding="utf-8") as a_file:
         print(a_file.read(), end="")
