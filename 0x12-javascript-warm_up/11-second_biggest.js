25043be2c64918b2841a2aa70e28038d33de3a1b
Haile-08 <haile4cmd@gmail.com>
2023-03-14 06:21:55 -0700

    Add array fucntion

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const argc = process.argv.length;
-if (argc <= 3) {
-  console.log('0');
-} else {
-  if (process.argv[2] < process.argv[3]) {
-    console.log(process.argv[3]);
-  } else {
-    console.log(process.argv[2]);
-  }
-}
+const args = process.argv
+  .slice(2)
+  .map(arg => Number.parseInt(arg))
+  .sort((a, b) => b - a);
+const val = args.length < 2 ? 0 : args[1];
+console.log(val);
