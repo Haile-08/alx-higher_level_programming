92bd11f2310d63f0ec27d5311935e73cbc1d9d5f
Haile-08 <haile4cmd@gmail.com>
2023-03-14 02:28:29 -0700

    If only one argument is passed to the script, print “Argument found” Otherwise, print “Arguments found”

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const num = process.argv.length;
-if (num <= 2) {
+const argc = process.argv.length;
+
+if (argc <= 2) {
   console.log('No argument');
 } else {
-  console.log('Argument found');
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 }
