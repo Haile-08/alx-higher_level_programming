5fef3b51047c237b483ed34d2d8e99efe8ee6fa9
Haile-08 <haile4cmd@gmail.com>
2023-03-17 01:52:06 -0700

    Fix the pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], password=argv[2],
                          database=argv[3], host='localhost', port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name\
+                 LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
