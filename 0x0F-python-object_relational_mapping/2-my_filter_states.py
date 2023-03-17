6a8a7428898fe7f90aa26c9efd93e0e9be4972d8
Haile-08 <haile4cmd@gmail.com>
2023-03-17 02:07:53 -0700

    Fix the pycodesyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3307, user=argv[1],
                          password=argv[2], database=argv[3])
-    cur= db.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
                  '{}' ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
